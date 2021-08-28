64c4bda980229c5b7785025c294f33e520282335
aayushali <aayushkhan51@gmail.com>
2021-08-28 13:50:20 +0545

    first commit

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>IT List | About</title>
+        <meta name="keywords" content="itlist" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsum ullam
+          repellat porro laudantium accusantium qui odio vel ea, totam soluta!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt unde
+          ipsa ipsam non temporibus error possimus illum pariatur dolores
+          facere.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas
+          explicabo consequuntur delectus consequatur itaque culpa, corporis
+          sapiente totam reprehenderit quae ea sed nulla quisquam praesentium,
+          debitis omnis, id vel officiis porro minus libero neque sequi? Fugit
+          labore animi eveniet earum vitae aspernatur fugiat maiores,
+          accusantium, repudiandae, officia sapiente fuga eos!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
